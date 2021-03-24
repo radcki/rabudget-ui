ba2c1a76fe6538715d6006f853e8485365a80c76
radcki <radekadamczewski@gmail.com>
2021-03-24 21:14:33 +0100

    silent auth renew

---
+++ src/pages/auth/SilentRenew.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+import { vuexOidcProcessSilentSignInCallback } from 'vuex-oidc';
+
+@Component
+export default class App extends Vue {
+  created() {
+    vuexOidcProcessSilentSignInCallback();
+  }
+}
+</script>
