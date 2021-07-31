51041cff4c827c9f69949f7cd18a1b893eb134bb
radcki <radekadamczewski@gmail.com>
2021-07-31 15:30:06 +0200

    improvements for category management

--- src/typings/api/budgetCategories/UpdateBudgetedAmountAmount.ts
+++ src/typings/api/budgetCategories/UpdateBudgetedAmountAmount.ts
@@ -1,3 +1,4 @@
+import { eBudgetCategoryType } from '@/typings/enums/eBudgetCategoryType';
 import { MoneyAmount } from '@/typings/MoneyAmount';
 import { SingleResponse } from '../baseTypes/SingleResponse';
 
@@ -6,4 +7,23 @@ export interface Command {
   amount: MoneyAmount;
 }
 
-export interface Result extends SingleResponse<MoneyAmount> {}
+export interface Result extends SingleResponse<BudgetCategoryDto> {}
+
+export interface BudgetCategoryDto {
+  budgetCategoryId: string;
+  budgetCategoryIconKey: string;
+  budgetCategoryIconId: string;
+  budgetId: string;
+  budgetCategoryType: eBudgetCategoryType;
+  order: number;
+  name: string;
+  currentBudgetedAmount: MoneyAmount;
+  budgetedAmounts: BudgetedAmountDto[];
+}
+
+export interface BudgetedAmountDto {
+  budgetedAmountId: string;
+  validFrom: Date;
+  validTo: Date | null;
+  amount: MoneyAmount;
+}
